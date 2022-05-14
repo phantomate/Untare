d08418c09c053f288ddd6cd420c276ca6c0d480a
Tom <tm.ch@mailbox.org>
2022-05-14 14:24:21 +0200

    Add supermarket category edit and delete dialogs

---
+++ lib/components/dialogs/delete_supermarket_category_dialog.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:tare/blocs/shopping_list/shopping_list_bloc.dart';
+import 'package:tare/blocs/shopping_list/shopping_list_event.dart';
+import 'package:tare/models/supermarket_category.dart';
+import 'package:tare/services/api/api_supermarket_category.dart';
+
+Future deleteSupermarketCategoryDialog(BuildContext context) async {
+  final _formBuilderKey = GlobalKey<FormBuilderState>();
+  final ShoppingListBloc _shoppingListBloc = BlocProvider.of<ShoppingListBloc>(context);
+  final ApiSupermarketCategory _apiSupermarketCategory =  ApiSupermarketCategory();
+  final List<SupermarketCategory> supermarketCategories = await _apiSupermarketCategory.getSupermarketCategories();
+  final List<DropdownMenuItem> supermarketCategoriesWidgetList =
+  supermarketCategories.map((category) => DropdownMenuItem(
+    value: category,
+    child: Text(category.name),
+  )).toList();
+
+  return showDialog(context: context, builder: (BuildContext dContext){
+    return Dialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        insetPadding: EdgeInsets.all(20),
+        child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: FormBuilder(
+                key: _formBuilderKey,
+                autovalidateMode: AutovalidateMode.disabled,
+                child: Wrap(
+                  spacing: 15,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: Text('Delete supermarket category', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17)),
+                    ),
+                    FormBuilderDropdown(
+                      name: 'category',
+                      allowClear: true,
+                      items: supermarketCategoriesWidgetList,
+                      decoration: InputDecoration(
+                          label: Text('Category')
+                      ),
+                      validator: FormBuilderValidators.compose(
+                          [FormBuilderValidators.required()]
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Container(
+                        alignment: Alignment.bottomRight,
+                        child: TextButton(
+                            onPressed: () {
+                              _formBuilderKey.currentState!.save();
+
+                              if (_formBuilderKey.currentState!.validate()) {
+                                _shoppingListBloc.add(DeleteSupermarketCategory(supermarketCategory: _formBuilderKey.currentState!.value['category']));
+                                Navigator.pop(dContext);
+                              }
+                            },
+                            child: Text('Delete')
+                        )
+                    )
+                  ],
+                )
+            )
+        )
+    );
+  });
+}
---
+++ lib/components/dialogs/edit_supermarket_category_dialog.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:tare/blocs/shopping_list/shopping_list_bloc.dart';
+import 'package:tare/blocs/shopping_list/shopping_list_event.dart';
+import 'package:tare/models/supermarket_category.dart';
+import 'package:tare/services/api/api_supermarket_category.dart';
+
+Future editSupermarketCategoryDialog(BuildContext context) async {
+  final _formBuilderKey = GlobalKey<FormBuilderState>();
+  final ShoppingListBloc _shoppingListBloc = BlocProvider.of<ShoppingListBloc>(context);
+  final ApiSupermarketCategory _apiSupermarketCategory = ApiSupermarketCategory();
+  final List<SupermarketCategory> supermarketCategories = await _apiSupermarketCategory.getSupermarketCategories();
+  final List<DropdownMenuItem> supermarketCategoriesWidgetList =
+    supermarketCategories.map((category) => DropdownMenuItem(
+      value: category,
+      child: Text(category.name),
+    )).toList();
+  bool isVisible = false;
+
+  return showDialog(context: context, builder: (BuildContext dContext){
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      insetPadding: EdgeInsets.all(20),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
+        child: FormBuilder(
+          key: _formBuilderKey,
+          autovalidateMode: AutovalidateMode.disabled,
+          child: StatefulBuilder(
+            builder: (context, setState) {
+              return Wrap(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Text('Edit supermarket category', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17)),
+                  ),
+                  FormBuilderDropdown(
+                    name: 'category',
+                    allowClear: true,
+                    items: supermarketCategoriesWidgetList,
+                    decoration: InputDecoration(
+                        label: Text('Category')
+                    ),
+                    validator: FormBuilderValidators.compose(
+                        [FormBuilderValidators.required()]
+                    ),
+                    onChanged: (dynamic value) {
+                      print(value);
+                      if (!isVisible) {
+                        setState(() {
+                          print('dude');
+                          isVisible = true;
+                        });
+                      }
+                    },
+                  ),
+                  Visibility(
+                    visible: isVisible,
+                      child: Column(
+                        children: [
+                          SizedBox(height: 15),
+                          FormBuilderTextField(
+                            name: 'name',
+                            decoration: InputDecoration(
+                                label: Text('New name')
+                            ),
+                            validator: FormBuilderValidators.compose([
+                              FormBuilderValidators.required(),
+                              FormBuilderValidators.max(128),
+                            ]),
+                          )
+                        ],
+                      )
+                  ),
+                  SizedBox(height: 15),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    child: TextButton(
+                      onPressed: isVisible ? () {
+                        _formBuilderKey.currentState!.save();
+
+                        if (_formBuilderKey.currentState!.validate()) {
+                          SupermarketCategory category = _formBuilderKey.currentState!.value['category'];
+                          String newName = _formBuilderKey.currentState!.value['name'];
+
+                          SupermarketCategory newSupermarketCategory = category.copyWith(name: newName);
+                          _shoppingListBloc.add(UpdateSupermarketCategory(supermarketCategory: newSupermarketCategory));
+                          Navigator.pop(dContext);
+                        }
+                      } : null,
+                      child: Text('Edit')
+                    )
+                  )
+                ],
+              );
+            }
+          )
+        )
+      )
+    );
+  });
+}
