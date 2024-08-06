7f6e0748373558d4ac5badb412ef1435dcf1a085
Alexander Linder <seine@eloque.nz>
2024-08-06 11:54:07 +0200

    Replace hardcoded colors on recipes list page

--- lib/components/recipes/recipe_list_component.dart
+++ lib/components/recipes/recipe_list_component.dart
@@ -65,12 +65,12 @@ Widget lastCooked(Recipe recipe, BuildContext context) {
         Icon(
           Icons.restaurant_outlined,
           size: 12,
-          color: (Theme.of(context).brightness.name == 'light') ? Colors.grey[600] : Colors.grey
+          color: Theme.of(context).colorScheme.secondary.withOpacity(0.8)
         ),
         const SizedBox(width: 2),
         Text(
             DateFormat('dd.MM.yy').format(DateTime.parse(recipe.lastCooked!).toLocal()),
-            style: TextStyle(color: (Theme.of(context).brightness.name == 'light') ? Colors.grey[600] : Colors.grey, fontSize: 12)
+            style: TextStyle(color: Theme.of(context).colorScheme.secondary.withOpacity(0.8), fontSize: 12)
         ),
         const SizedBox(width: 8)
       ],
@@ -87,7 +87,7 @@ Widget rating (Recipe recipe, BuildContext context) {
             recipe.rating.toString(),
             style: TextStyle(
               fontSize: 12,
-              color: (Theme.of(context).brightness.name == 'light') ? Colors.grey[600] : Colors.grey
+              color: Theme.of(context).colorScheme.secondary.withOpacity(0.8)
             )
         ),
         const SizedBox(width: 2),
