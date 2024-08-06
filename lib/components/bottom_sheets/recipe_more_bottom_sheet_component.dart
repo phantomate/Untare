bb092da75841934e6b5bce2f8c10907960ed0932
Alexander Linder <seine@eloque.nz>
2024-08-06 08:12:37 +0200

    Remove bottom sheet name

--- lib/components/bottom_sheets/recipe_more_bottom_sheet_component.dart
+++ lib/components/bottom_sheets/recipe_more_bottom_sheet_component.dart
@@ -10,23 +10,6 @@ Future recipeMoreBottomSheet(BuildContext context, Recipe recipe) {
       builder: (btsContext) => Wrap(
         spacing: 15,
         children: [
-          Container(
-            height: 44,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-                borderRadius: const BorderRadius.vertical(top: Radius.circular(10)),
-            ),
-            child: Text(
-              recipe.name,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18
-              ),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
           buildRecipeMore(context, btsContext, recipe)
         ],
       ),
