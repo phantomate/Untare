1b91d16382dacf7a8c058002ade6dc3b6cde5004
Alexander Linder <seine@eloque.nz>
2024-08-06 10:08:25 +0200

    Migrate foods page

--- lib/pages/foods_page.dart
+++ lib/pages/foods_page.dart
@@ -151,7 +151,7 @@ class FoodsPageState extends State<FoodsPage> {
                             ),
                           ),
                           separatorBuilder: (context, index) => Divider(
-                            color: (Theme.of(context).brightness.name == 'light') ? Colors.grey[300]! : Colors.grey[700]!,
+                            color: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
                             height: 4,
                           ),
                           itemCount: foods.length
